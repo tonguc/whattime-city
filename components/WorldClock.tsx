fe43a6edb1f10fdd1ba8597fd45aa028f69266d3
tonguc <tkaracay@gmail.com>
2025-12-10 00:33:03 +0300

    Add files via upload

--- components/WorldClock.tsx
+++ components/WorldClock.tsx
@@ -599,7 +599,7 @@ export default function WorldClock({ initialCity }: WorldClockProps) {
             </div>
           )}
           
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
             {getCitiesByContinent(selectedContinent)
               .filter(city => 
                 selectedContinent === 'all' || !continentFilter ||
