ef34fbe07a08f5211945cc552d9e8fbc79ab6a39
tonguc <tkaracay@gmail.com>
2025-12-21 01:14:56 +0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,12 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9352058741490976"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="antialiased">
         <CityProvider>
