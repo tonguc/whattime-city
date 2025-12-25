055b58dc3ef884188dea1ff53f78a3dc3a815adf
tonguc <tkaracay@gmail.com>
2025-12-25 16:25:42 +0300

    24cities

--- components/WorldClock/WorldCitiesGrid.tsx
+++ components/WorldClock/WorldCitiesGrid.tsx
@@ -97,7 +97,7 @@ export default function WorldCitiesGrid({
         </div>
       )}
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
         {filteredCities.map((city) => (
           <CityCard
             key={city.slug}
