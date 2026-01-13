9385c5923f33c9a86dfe82b4bdf58be56d373b6e
tonguc <tkaracay@gmail.com>
2026-01-13 18:04:25 +0300

    vvvv

--- app/country/[country]/opengraph-image.tsx
+++ app/country/[country]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from 'next/og'
 import { getCountryBySlug } from '@/lib/cities'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 export const alt = 'Country Time Zones'
 export const size = { width: 1200, height: 630 }
 export const contentType = 'image/png'
